5b6efd4aff1dc9ff9b4cdaec092ddfb90df45afb
rohanta-010 <rohanta.dev@gmail.com>
2026-08-05 15:55:27 +0530

    Fix comment formatting in 01_input.py

--- 1.Python Fundamentals/3.Taking User Input/01_input.py
+++ 1.Python Fundamentals/3.Taking User Input/01_input.py
@@ -9,6 +9,6 @@ print(c)
 print(type(c))
 print(type(int(c)))
 
-# always the user input is :
+## always the user input is :
 # an optional parameter that is used to display a message to the user before taking input. It can be used to provide instructions or context for the input being requested.
 # taken as string, so we need to typecast it to integer if we want to perform any mathematical operation on it.
