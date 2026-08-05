cc004dcf0378840e6a0662c0757ee62294bb5f65
rohanta-010 <rohanta.dev@gmail.com>
2026-08-05 09:25:05 +0530

    Updated comments

--- 1.Python Fundamentals/1.Variables and Data Types/02_data types.py
+++ 1.Python Fundamentals/1.Variables and Data Types/02_data types.py
@@ -1,12 +1,14 @@
-# Python supports several built-in data types:
-#     Integers (int): Whole numbers (e.g., 10, -5).
-#     Floats (float): Decimal numbers (e.g., 3.14, -0.001).
-#     Strings (str): Text data enclosed in quotes (e.g., "Hello", 'Python').
-#     Booleans (bool): Represents True or False.
-#     Lists: Ordered, mutable collections (e.g., [1, 2, 3]).
-#     Tuples: Ordered, immutable collections (e.g., (1, 2, 3)).
-#     Sets: Unordered collections of unique elements (e.g., {1, 2, 3}).
-#     Dictionaries: Key-value pairs (e.g., {"name": "Alice", "age": 25}).
+## Python supports several built-in data types:
+# Integers (int): Whole numbers (e.g., 10, -5).
+# Floats (float): Decimal numbers (e.g., 3.14, -0.001).
+# Strings (str): Text data enclosed in quotes (e.g., "Hello", 'Python').
+# Booleans (bool): Represents True or False.
+# Lists: Ordered, mutable collections (e.g., [1, 2, 3]).
+# Tuples: Ordered, immutable collections (e.g., (1, 2, 3)).
+# Sets: Unordered collections of unique elements (e.g., {1, 2, 3}).
+# Dictionaries: Key-value pairs (e.g., {"name": "Alice", "age": 25}).
+
+# example 
 
 age = 3 
 print(age)
@@ -24,4 +26,4 @@ is_completed = True # can also be False
 print(is_completed) # if we just type (ic), vs code will auto suggest the variable name
 print(type(is_completed))
 
-#In Python, everything is an object, and categories of objects are called classes.
+## In Python, everything is an object, and categories of objects are called classes.
