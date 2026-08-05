74a244e629e7415a2633263ca80a99ede8655b28
rohanta-010 <rohanta.dev@gmail.com>
2026-08-05 09:19:03 +0530

    Updated comments

--- 1.Python Fundamentals/1.Variables and Data Types/01_basics.py
+++ 1.Python Fundamentals/1.Variables and Data Types/01_basics.py
@@ -1,10 +1,10 @@
-# In Python, variables are used to store data that can be used and manipulated throughout a program. A variable is created the moment you assign a value to it using the assignment operator (=).
+## In Python, variables are used to store data that can be used and manipulated throughout a program. A variable is created the moment you assign a value to it using the assignment operator (=).
 
 age = 34 # integer
 name = "Harry" # string
 cgpa = 4.55 # float
 
-# Rule of defining a variable in Python  
+## Rule of defining a variable in Python  
 # Variable names must start with a letter (a-z, A-Z) or an underscore (_).
 # They can contain letters, numbers, and underscores.
 # Variable names are case-sensitive (age and Age are different).
@@ -18,7 +18,7 @@ __nice_45 = 34
 a_b_c_7 = "Sam"
 
 
-# Every Python program is loaded into (RAM)memory before execution(i.e line by line), and variables are references to objects stored in memory.
+## Every Python program is loaded into (RAM)memory before execution(i.e line by line), and variables are references to objects stored in memory.
 ## x = 10
 # Python does not simply store x as a box containing 10. Instead:
 # 10 is created as an object in memory
