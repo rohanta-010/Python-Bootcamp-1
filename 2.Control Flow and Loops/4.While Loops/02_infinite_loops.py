4e675ae7d168fc5446a19fcddbd2c9ecc0fd31d5
rohanta-010 <rohanta.dev@gmail.com>
2026-08-06 11:23:51 +0530

    Clarify comment on infinite loop in while statement

--- 2.Control Flow and Loops/4.While Loops/02_infinite_loops.py
+++ 2.Control Flow and Loops/4.While Loops/02_infinite_loops.py
@@ -1,4 +1,4 @@
 i = 1
-while True: #while True: creates an infinite loop because the condition is always True.
+while True: #while True: creates an infinite loop because the condition is always True. i.e while condition
     print(i)
     i = i + 1 
