ad2cc620daba369d529117e9f2497df96aef5b0a
rohanta-010 <rohanta.dev@gmail.com>
2026-08-06 11:22:56 +0530

    Modify for loop and enhance comments
    Updated comment for clarity and added multiplication loop.

--- 2.Control Flow and Loops/3.For Loops/01_for_loops.py
+++ 2.Control Flow and Loops/3.For Loops/01_for_loops.py
@@ -4,7 +4,7 @@
 # print(4)
 # print(5)
 
-# for i in range(1, 6): # range function goes from 1 to (6-1) ie 5 in this case (n,n-1)
+# for i in range(1, 6): # range function goes from 1 to (6-1) i.e 5 in this case (n,n-1)
 #     print(i)
 
 for i in range(1, 11):
