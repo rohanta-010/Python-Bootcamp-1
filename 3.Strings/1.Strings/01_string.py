2fb6c7e421d5497f6c4b9e8cfdb7b52183c54bde
rohanta-010 <rohanta.dev@gmail.com>
2026-08-10 17:43:47 +0530

    Update 01_string.py

--- 3.Strings/1.Strings/01_string.py
+++ 3.Strings/1.Strings/01_string.py
@@ -1,6 +1,9 @@
 name1 = "Rohan"
 name2 = 'Rohan'
-name3 = '''Rohan is a good boy''' # multi-line string
+
+multi-line string
+name3 = '''Rohan is a good boy'''
+
 print(name1)
 print(name2)
 print(name3)
