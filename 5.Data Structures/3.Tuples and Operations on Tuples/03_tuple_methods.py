b65f0c3aa6be2e9293e5fc04bfc42d68664c50ad
Rohan T A <rohanta.dev@gmail.com>
2026-08-12 23:11:33 +0530

    Updated Tuples

--- 5.Data Structures/3.Tuples and Operations on Tuples/03_tuple_methods.py
+++ 5.Data Structures/3.Tuples and Operations on Tuples/03_tuple_methods.py
@@ -1,4 +1,5 @@
 t = (3, 12, 1, 54, 23, 12)
+#   (0,  1, 2,  3,  4,  5)
 
 print(t.count(12)) # here we are counting the number of times 12 appears in the tuple t
-print(t.index(12)) # here we are finding the index of the first occurrence of 12 in the tuple t
+print(t.index(3)) # here we are finding the index of the first occurrence of 3 in the tuple t
