0752153396076a5f926f76e735890b99fbaab695
Rohan T A <rohanta.dev@gmail.com>
2026-08-19 01:35:36 +0530

    Removed the Tuples folder

--- 5.Data Structures/3.Tuples and Operations on Tuples/01_tuples.py
+++
@@ -1,7 +0,0 @@
-a = (3, 2, 22, 13)
-
-print(a)
-print(a[2])
-a[3] = 32 # TypeError: 'tuple' object does not support item assignment
-
-b = (3, ) # This is a tuple with a single element. The comma is necessary to distinguish it from a regular parenthesis.
