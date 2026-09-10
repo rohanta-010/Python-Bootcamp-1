51985618f5f89c80a0cab3641c444bd264683f5f
rohanta-010 <rohanta.dev@gmail.com>
2026-09-10 16:57:53 +0530

    Enhance comments in merge_dicts function
    Added comments to clarify the dictionary merging process.

--- 5.Data Structures/Practice Set/Practice Set_ans/problem6.3.py
+++ 5.Data Structures/Practice Set/Practice Set_ans/problem6.3.py
@@ -11,6 +11,8 @@ def merge_dicts(dict1, dict2):
         dict: A new dictionary with combined key-value pairs
     '''
     return {**dict1, **dict2}
+    # Create a new dictionary and unpack all key-value pairs from dict1 and dict2 into it.
+    # handles positional/iterable elements, while ** handles mappings/key-value pairs.
 
 d1 = {"a": 1, "b": 2}
 d2 = {"c": 3, "d": 4}
