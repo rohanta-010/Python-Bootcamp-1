b092f94e632b350d28c01aeb9d23412351cffdf8
rohanta-010 <rohanta.dev@gmail.com>
2026-09-16 14:38:08 +0530

    Enhance Employee class with constructor comments
    Added comments to the Employee class to explain the constructor and its parameters.

--- 6.OOP/2.Constructors/01_constructors.py
+++ 6.OOP/2.Constructors/01_constructors.py
@@ -1,8 +1,15 @@
 class Employee: 
-
+    
+    # __init__() is where we set up the initial data of each object, and self tells Python which particular object that data belongs to.
     def __init__(self, salary, name, bond):
+        # `self` refers to the current object.
+        # `salary`, 'name' and `bond` are parameters that receive values when we create the object.
+        
         self.salary = salary # Create an instance attribute of name salary and assign it with salary
         self.name = name 
+        #   Put this name value into the current object's name attribute.
+        #   self.name → attribute belonging to the object
+        #   name      → parameter value
         self.bond = bond
 
     def get_salary(self):
@@ -13,5 +20,5 @@ class Employee:
     
 
 e1 = Employee(34000, "John Doe", 4)
-# print(e1.get_salary())
+print(e1.get_salary())
 e1.get_info()
