6a4a416744a89dee05910bd863ed9ae2dad7aa7b
rohanta-010 <rohanta.dev@gmail.com>
2026-09-17 11:54:30 +0530

    Enhance comments in operator overloading example
    Updated comments for clarity on the sum method and operator overloading.

--- 6.OOP/5.Method Overriding and Opeartor Overloading/01_operator_overloading.py
+++ 6.OOP/5.Method Overriding and Opeartor Overloading/01_operator_overloading.py
@@ -3,8 +3,8 @@ class Point:
         self.x = x 
         self.y = y
 
-    def sum(self, p):
-        return Point((self.x + p.x), (self.y + p.y))
+    def sum(self, p): # self refers to the object calling the method => Point.sum(p1, p2)
+        return Point((self.x + p.x), (self.y + p.y)) # We're creating a new Point object: Point(9, 5)
     
     def print_point(self):
         print(f"X is {self.x} and Y is {self.y}")
@@ -15,6 +15,8 @@ class Point:
 p1 = Point(3, 2)
 p2 = Point(6, 3)
 
-# p = p1.sum(p2) # Returns a new point which is sum of p1 and p2
+p = p1.sum(p2) # Returns a new point which is sum of p1 and p2, where p is the third (3rd) point object : p  → (9, 5)
+p.print_point()
+print("*)
 p = p1 + p2 # We overloaded the + Operator by writing __add__ function
 p.print_point()
