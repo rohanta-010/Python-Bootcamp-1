e082dca77068eba4cb9699a606ad9874135702d4
rohanta-010 <rohanta.dev@gmail.com>
2026-09-17 12:07:01 +0530

    Update 01_operator_overloading.py

--- 6.OOP/5.Method Overriding and Opeartor Overloading/01_operator_overloading.py
+++ 6.OOP/5.Method Overriding and Opeartor Overloading/01_operator_overloading.py
@@ -9,14 +9,14 @@ class Point:
     def print_point(self):
         print(f"X is {self.x} and Y is {self.y}")
 
-    def __add__(self, p):
-        return Point((self.x + p.x), (self.y + p.y))
+    def __add__(self, p): # This is a special/dunder method. p = p1.__add__(p2) -> Point.__add__(p1, p2)
+        return Point((self.x + p.x), (self.y + p.y)) # creates a new Point object
 
 p1 = Point(3, 2)
 p2 = Point(6, 3)
 
 p = p1.sum(p2) # Returns a new point which is sum of p1 and p2, where p is the third (3rd) point object : p  → (9, 5)
 p.print_point()
-print("*)
-p = p1 + p2 # We overloaded the + Operator by writing __add__ function
+print("*")
+p = p1 + p2 # We overloaded the + Operator by writing __add__ function, __add__() lets you define what the + operator should do when it is used with objects of your class. Giving an operator additional behavior for your own objects.
 p.print_point()
